9391080c46df805561e65b0d686f353c42e4e949
CatHat <liase810@gmail.com>
2024-05-29 20:31:01 +0200

    Added Waves and Hearts

--- dist/main.js
+++ dist/main.js
@@ -1,26 +1,42 @@
 "use strict";
-let width = Math.round(window.innerWidth / 100);
-let height = Math.round(window.innerHeight / 100);
+const CELL_WIDTH = 50;
+const CELL_HEIGHT = 50;
+const TIME_STEP_MS = 50;
+const PATTERN = [
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+];
+let width = Math.round(window.innerWidth / CELL_WIDTH);
+let height = Math.round(window.innerHeight / CELL_HEIGHT);
 const grid = document.getElementsByClassName('grid')[0];
-const grid2D = arrayTo2D(new Array(width * height).fill(null), width, height);
+let infoGrid2D;
+makeGrid();
+let heartArray;
+makeHeart();
 addNewButtons(grid, width, height);
-let wave = setInterval(() => { }, 1000);
 window.addEventListener('resize', () => {
-    width = Math.round(window.innerWidth / 100);
-    height = Math.round(window.innerHeight / 100);
+    width = Math.round(window.innerWidth / CELL_WIDTH);
+    height = Math.round(window.innerHeight / CELL_HEIGHT);
     addNewButtons(grid, width, height);
+    makeGrid();
 });
 function addNewButtons(grid, width, height) {
     grid.style.gridTemplateColumns = `repeat(${width}, auto)`;
     grid.style.gridTemplateRows = `repeat(${height}, auto)`;
     removeAllChildren(grid);
     for (let i = 0; i < width * height; i++) {
-        const button = createButton();
+        const button = createButton(i % width, Math.floor(i / width));
         const pinkOrCream = (i + (width % 2 === 0 ? Math.floor(i / width) : 0)) % 2 === 0;
-        button.childNodes.forEach((element) => {
-            const htmlelement = element;
-            htmlelement.classList.add(pinkOrCream ? 'pink' : 'cream');
-        });
         button.classList.add(pinkOrCream ? 'pink' : 'cream');
         button.id = i.toString();
         grid.appendChild(button);
@@ -28,46 +44,93 @@ function addNewButtons(grid, width, height) {
 }
 function removeAllChildren(element) {
     while (element.firstChild) {
-        if (element.lastChild) {
-            element.removeChild(element.lastChild);
+        element.removeChild(element.firstChild);
+    }
+    for (const y of infoGrid2D) {
+        for (const x of y) {
+            if (x.timeout !== null)
+                clearInterval(x.timeout);
         }
     }
 }
-function createButton() {
+function createButton(x, y) {
     const button = document.createElement('button');
-    button.addEventListener('click', changeColor);
+    if (x !== undefined && y !== undefined)
+        button.addEventListener('click', () => {
+            sendWave(x, y);
+        });
     return button;
 }
-function changeColor() {
+function changeColor(index) {
+    const gridChildren = grid.childNodes;
+    const x = index % width;
+    const y = Math.floor(index / width);
+    const pinkOrCream = heartArray[y][x] === 1;
+    gridChildren[index].classList.add(pinkOrCream ? 'cream' : 'pink');
+    gridChildren[index].classList.remove(pinkOrCream ? 'pink' : 'cream');
 }
-function sendWave(x, y, direction) {
-    const elementGrid2D = arrayTo2D(Array.from(grid.childNodes), width, height);
-    switch (direction) {
-        case 0:
-            break;
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-        case 6:
-            break;
-        case 7:
-            break;
+function sendWave(x, y) {
+    infoGrid2D[y][x].number += 1;
+    changeColor(x + width * y);
+    const indexes = [
+        { x: x + 1, y: y },
+        { x: x - 1, y: y },
+        { x: x, y: y + 1 },
+        { x: x, y: y - 1 },
+    ];
+    for (const index of indexes) {
+        if (index.x < 0 ||
+            index.y < 0 ||
+            index.x > width - 1 ||
+            index.y > height - 1)
+            continue;
+        if (infoGrid2D[y][x].number > infoGrid2D[index.y][index.x].number) {
+            if (infoGrid2D[index.y][index.x].timeout !== null)
+                clearTimeout(infoGrid2D[index.y][index.x].timeout);
+            infoGrid2D[index.y][index.x].timeout = setTimeout(() => {
+                sendWave(index.x, index.y);
+                infoGrid2D[index.y][index.x].timeout = null;
+            }, TIME_STEP_MS);
+        }
     }
 }
-function arrayTo2D(gridArray, width, height) {
+function arrayTo2DGrid(array, width, height) {
     const result = [];
     for (let y = 0; y < height; y++) {
         result.push([]);
         for (let x = 0; x < width; x++) {
-            result[y].push(gridArray[x + width * y]);
+            result[y].push(array[x + width * y]);
         }
     }
     return result;
+}
+function Grid2DToArray(grid) {
+    const result = [];
+    grid.forEach((row) => row.forEach((item) => result.push(item)));
+    return result;
+}
+function makeGrid() {
+    infoGrid2D = new Array(height);
+    for (let i = 0; i < infoGrid2D.length; i++) {
+        infoGrid2D[i] = new Array(width);
+        for (let j = 0; j < infoGrid2D[i].length; j++) {
+            infoGrid2D[i][j] = { number: 0, timeout: null };
+        }
+    }
+}
+function makeHeart() {
+    heartArray = new Array(height);
+    for (let i = 0; i < heartArray.length; i++) {
+        heartArray[i] = new Array(width);
+        for (let j = 0; j < heartArray[i].length; j++) {
+            heartArray[i][j] = 0;
+        }
+    }
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            console.log(x, y);
+            heartArray[y][x] =
+                PATTERN[y % PATTERN.length][x % PATTERN[y % PATTERN.length].length];
+        }
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,41 @@
-let width = Math.round(window.innerWidth / 100)
-let height = Math.round(window.innerHeight / 100)
+const CELL_WIDTH = 50
+const CELL_HEIGHT = 50
+const TIME_STEP_MS = 50
+
+const PATTERN = [
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+]
+
+let width = Math.round(window.innerWidth / CELL_WIDTH)
+let height = Math.round(window.innerHeight / CELL_HEIGHT)
 const grid = document.getElementsByClassName('grid')[0] as HTMLElement
-const grid2D = arrayTo2D(new Array(width * height).fill(null), width, height)
+
+let infoGrid2D: { number: number; timeout: number | null }[][]
+
+makeGrid()
+
+let heartArray: number[][]
+
+makeHeart()
 
 addNewButtons(grid, width, height)
 
-let wave = setInterval(() => {}, 1000)
-
 window.addEventListener('resize', () => {
-    width = Math.round(window.innerWidth / 100)
-    height = Math.round(window.innerHeight / 100)
+    width = Math.round(window.innerWidth / CELL_WIDTH)
+    height = Math.round(window.innerHeight / CELL_HEIGHT)
     addNewButtons(grid, width, height)
+    makeGrid()
 })
 
 function addNewButtons(grid: HTMLElement, width: number, height: number) {
@@ -20,15 +45,10 @@ function addNewButtons(grid: HTMLElement, width: number, height: number) {
     removeAllChildren(grid)
 
     for (let i = 0; i < width * height; i++) {
-        const button = createButton()
+        const button = createButton(i % width, Math.floor(i / width))
 
         const pinkOrCream =
             (i + (width % 2 === 0 ? Math.floor(i / width) : 0)) % 2 === 0
-
-        button.childNodes.forEach((element) => {
-            const htmlelement = element as HTMLElement
-            htmlelement.classList.add(pinkOrCream ? 'pink' : 'cream')
-        })
 
         button.classList.add(pinkOrCream ? 'pink' : 'cream')
 
@@ -43,16 +63,22 @@ function addNewButtons(grid: HTMLElement, width: number, height: number) {
 
 function removeAllChildren(element: HTMLElement) {
     while (element.firstChild) {
-        if (element.lastChild) {
-            element.removeChild(element.lastChild)
+        element.removeChild(element.firstChild)
+    }
+    for (const y of infoGrid2D) {
+        for (const x of y) {
+            if (x.timeout !== null) clearInterval(x.timeout)
         }
     }
 }
 
-function createButton(/*extraElements?: HTMLElement[]*/) {
+function createButton(x?: number, y?: number) {
     const button = document.createElement('button')
 
-    button.addEventListener('click', changeColor)
+    if (x !== undefined && y !== undefined)
+        button.addEventListener('click', () => {
+            sendWave(x, y)
+        })
 
     //const edge = document.createElement('span')
     //const front = document.createElement('span')
@@ -62,61 +88,115 @@ function createButton(/*extraElements?: HTMLElement[]*/) {
 
     //button.appendChild(edge)
     //button.appendChild(front)
-    //if (extraElements)
-    //    extraElements.forEach((element) => button.append(element))
 
     return button
 }
 
-function changeColor() {
-    //const gridChildren = grid.childNodes
-    //
-    //for (let gridChild of gridChildren) {
-    //    ;(gridChild as HTMLElement).childNodes.forEach((element) => {
-    //        const htmlelement = element as HTMLElement
-    //        ;['pink', 'cream'].forEach((color) => {
-    //            htmlelement.classList.toggle(color)
-    //        })
-    //    })
-    //}
-}
+function changeColor(index: number) {
+    const gridChildren = grid.childNodes
 
-function sendWave(x: number, y: number, direction?: number) {
-    const elementGrid2D: any[][] = arrayTo2D(
-        Array.from(grid.childNodes),
-        width,
-        height
+    const x = index % width
+    const y = Math.floor(index / width)
+
+    const pinkOrCream = heartArray[y][x] === 1
+
+    ;(gridChildren[index] as HTMLElement).classList.add(
+        pinkOrCream ? 'cream' : 'pink'
+    )
+    ;(gridChildren[index] as HTMLElement).classList.remove(
+        pinkOrCream ? 'pink' : 'cream'
     )
 
-    switch (direction) {
-        case 0:
-            break
-        case 1:
-            break
-        case 2:
-            break
-        case 3:
-            break
-        case 4:
-            break
-        case 5:
-            break
-        case 6:
-            break
-        case 7:
-            break
+    /*;['pink', 'cream'].forEach((color) => {
+        ;(gridChildren[index] as HTMLElement).classList.toggle(color)
+    })*/
+}
+
+function sendWave(x: number, y: number) {
+    infoGrid2D[y][x].number += 1
+    changeColor(x + width * y)
+    //;(grid.childNodes[x + width * y] as HTMLElement).setAttribute(
+    //    'before-data',
+    //    infoGrid2D[y][x].number.toString()
+    //)
+
+    const indexes = [
+        { x: x + 1, y: y },
+        { x: x - 1, y: y },
+        { x: x, y: y + 1 },
+        { x: x, y: y - 1 },
+    ]
+
+    for (const index of indexes) {
+        if (
+            index.x < 0 ||
+            index.y < 0 ||
+            index.x > width - 1 ||
+            index.y > height - 1
+        )
+            continue
+
+        if (infoGrid2D[y][x].number > infoGrid2D[index.y][index.x].number) {
+            if (infoGrid2D[index.y][index.x].timeout !== null)
+                clearTimeout(infoGrid2D[index.y][index.x].timeout!)
+
+            infoGrid2D[index.y][index.x].timeout = setTimeout(() => {
+                sendWave(index.x, index.y)
+                infoGrid2D[index.y][index.x].timeout = null
+            }, TIME_STEP_MS)
+        }
     }
 }
 
-function arrayTo2D(gridArray: any[], width: number, height: number) {
+function arrayTo2DGrid(array: any[], width: number, height: number) {
     const result: any[][] = []
 
     for (let y = 0; y < height; y++) {
         result.push([])
         for (let x = 0; x < width; x++) {
-            result[y].push(gridArray[x + width * y])
+            result[y].push(array[x + width * y])
         }
     }
 
     return result
+}
+
+function Grid2DToArray(grid: any[][]) {
+    const result: any[] = []
+
+    grid.forEach((row) => row.forEach((item) => result.push(item)))
+
+    return result
+}
+
+function makeGrid() {
+    infoGrid2D = new Array(height)
+
+    for (let i = 0; i < infoGrid2D.length; i++) {
+        infoGrid2D[i] = new Array(width)
+        for (let j = 0; j < infoGrid2D[i].length; j++) {
+            infoGrid2D[i][j] = { number: 0, timeout: null }
+        }
+    }
+}
+
+function makeHeart() {
+    heartArray = new Array(height)
+
+    for (let i = 0; i < heartArray.length; i++) {
+        heartArray[i] = new Array(width)
+        for (let j = 0; j < heartArray[i].length; j++) {
+            heartArray[i][j] = 0
+        }
+    }
+
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            console.log(x, y)
+            heartArray[y][x] =
+                PATTERN[y % PATTERN.length][
+                    x % PATTERN[y % PATTERN.length].length
+                ]
+        }
+    }
 }
